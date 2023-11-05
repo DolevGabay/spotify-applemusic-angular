11b8e8a361b415283afc8db6985789217f160798
Matan Tsach <matan.tsach@gmail.com>
2023-11-05 16:15:23 +0200

    Changes #11

--- server/transfer.js
+++ server/transfer.js
@@ -14,16 +14,17 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    console.log(db)
-    const collection = await db.collection('transfers');
+   // console.log(db)
+   // const collection = await db.collection('transfers');
 
     const { transferData } = req.body;
-    const source = req.session.streamers.source.streamer;
-    const dest = req.session.streamers.dest.streamer;
+   // const source = req.session.streamers.source.streamer;
+   // const dest = req.session.streamers.dest.streamer;
     
-    const result = await collection.insertOne({ transferData, source, dest});
+   //const result = await collection.insertOne({ transferData, source, dest});
     req.session.transferData = transferData;
-    res.send(result.insertedId).status(201);
+   // res.send(result.insertedId).status(201);
+    res.sendStatus(201);
 });
 
 router.delete('/', (req, res) => {

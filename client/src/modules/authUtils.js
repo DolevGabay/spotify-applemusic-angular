cf7313752f43f49f7da9e37621f4bee2b9a5c90f
Matan Tsach <matan.tsach@gmail.com>
2023-12-11 15:31:21 +0200

    Reverting from proxy

--- client/src/modules/authUtils.js
+++ client/src/modules/authUtils.js
@@ -10,7 +10,7 @@ export async function isAuthed(streamer) {
   console.log("entering isAuthed");
   try {
     console.log("entering try");
-    const response = await axios.get(`/api/${streamer}/token`, {
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND_API}/${streamer}/token`, {
       withCredentials: true,
     });
     console.log(response);

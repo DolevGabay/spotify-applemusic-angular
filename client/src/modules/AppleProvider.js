ca5d5ed2c3f94dd79ab5a3a16bbe718ddbe3c42c
Dolev <dolevg621@gmail.com>
2023-12-19 14:51:46 +0200

    also didnt work

--- client/src/modules/AppleProvider.js
+++ client/src/modules/AppleProvider.js
@@ -191,11 +191,10 @@ class AppleProvider {
   async loadProfile() {
     await this.configure();
     console.log('Configured');
-  
     if (!this.isLoggedIn()) {
       try {
         console.log('loadProfile try');
-        const res = await this.instance.getInstance().authorizeWithoutPopup();
+        const res = await this.instance.getInstance().authorize();
         console.log('res', res);
         console.log('loaded', this.instance.getInstance().musicUserToken);
       } catch (error) {
@@ -203,14 +202,13 @@ class AppleProvider {
         return;
       }
     }
-  
+
     this.header = {
-      Authorization: `Bearer ${this.accessToken}`,
-      "Music-User-Token": this.getMusicInstance().musicUserToken,
-      "Content-Type": "application/json",
-    };
+        Authorization: `Bearer ${this.accessToken}`,
+        "Music-User-Token": this.getMusicInstance().musicUserToken,
+        "Content-Type": "application/json",
+      };
   }
-  
 
   async configure() {
     this.instance.configure({

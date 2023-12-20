e75d5410de354fbfc4ee3a042a4efb7da9546096
Matan Tsach <matan.tsach@gmail.com>
2023-12-20 13:09:55 +0200

    Added rate-limiter

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const auth = require('./routes/auth');
 const transfers = require('./routes/transfers');
 const nodemailer = require('nodemailer');
 const redis = require('redis');
+const rateLimit = require("express-rate-limit");
 const RedisStore = require("connect-redis").default;
 
 
@@ -16,36 +17,51 @@ const RedisStore = require("connect-redis").default;
 const PORT = process.env.PORT || 8888;
 const app = express();
 
+// Rate Limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 500 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
 // Session
-if (process.env.NODE_ENV === 'production') {
-  // Redis
+const configureSession = () => {
+  const sessionConfig = {
+    secret: 'matandolev', // Replace with your secret key
+    resave: false,
+    saveUninitialized: false
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    const redisClient = createRedisClient();
+    app.set('trust proxy', 1);
+    sessionConfig.cookie = configureProductionCookie();
+    sessionConfig.store = new RedisStore({ client: redisClient });
+  }
+
+  app.use(session(sessionConfig));
+};
+
+const createRedisClient = () => {
   const redisClient = redis.createClient({
     url: process.env.REDIS_URL // Redis URL from environment variable
   });
   redisClient.on('error', (err) => console.log('Redis Client Error', err));
   redisClient.connect();
 
-  app.set('trust proxy', 1);
-  app.use(session({
-    secret: 'matandolev', // Replace with your secret key
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: true,
-      httpOnly: true,
-      domain: `.${process.env.FRONTEND_BASE_URI}`,
-      sameSite: 'none',
-      maxAge: 60 * 60 * 24 * 1000
-    },
-    store: new RedisStore({ client: redisClient })
-  }));
-} else {
-  app.use(session({
-    secret: 'matandolev', // Replace with your secret key
-    resave: false,
-    saveUninitialized: false,
-  }));
-}
+  return redisClient;
+};
+
+const configureProductionCookie = () => {
+  return {
+    secure: true,
+    httpOnly: true,
+    domain: `.${process.env.FRONTEND_BASE_URI}`,
+    sameSite: 'none',
+    maxAge: 60 * 60 * 24 * 1000
+  };
+};
+configureSession();
 
 // Cors
 const corsOptions = {
